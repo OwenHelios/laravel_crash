bdf127f7ac79ff7b7c753951e813de7dd3cd2e3a
OwenHelios <owenhelios@outlook.com>
2022-05-04 08:57:40 +0800

    middleware

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    // Show register/create form
+    public function create(){
+        return view('users.register');
+    }
+
+    // Create new user
+    public function store(Request $request){
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'], // at least 3 characters
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'confirmed', 'min:6']
+        ]);
+
+        // Hash password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        // Create user
+        $user = User::create($formFields);
+
+        // Login
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'User created and logged in');
+    }
+
+    // Logout user
+    public function logout(Request $request){
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message', 'User logged out');
+    }
+
+    // Show login form
+    public function login(){
+        return view('users.login');
+    }
+
+    // Authenticate user
+    public function authenticate(Request $request){
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($formFields)){
+            $request->session()->regenerate();
+            return redirect('/')->with('message', 'Logged in successfully');
+        }
+        return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -29,24 +30,37 @@ use App\Models\Listing;
 Route::get('/', [ListingController::class, 'index']);
 
 // Show create form
-Route::get('/listings/create', [ListingController::class, 'create']);
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
 
 // Store listing data
-Route::post('/listings', [ListingController::class, 'store']);
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
 
 // Show edit form
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
 
 // Update listing data
-Route::put('/listings/{listing}', [ListingController::class, 'update']);
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
 
 // Delete listing data
-Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
 
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
+// Show register/create form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
+// Create new user
+Route::post('/users', [UserController::class, 'store']);
+
+// Log out user
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show login form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Log in user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
 
 // Route::get('/hello', function () {
 //     return response("<h1>Hello</h1>", 200)
